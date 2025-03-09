510306006c36208ed4c63750521864a6a6b46916
Amanvyas00 <140067703+Amanvyas00@users.noreply.github.com>
2025-03-09 23:24:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Registration from './component/Registration/Registration.jsx'
+// import Index from './index.css';
+
+function App() {
+  return (
+    <>
+      <Registration/>
+    </>
+  );
+}
+
+export default App;
